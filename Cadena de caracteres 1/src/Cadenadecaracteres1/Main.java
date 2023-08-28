a9d858a396569d038daa40814330f955f64493fd
CerutiFranco <131700000+CerutiFranco@users.noreply.github.com>
2023-08-28 14:44:54 -0300

    Add files via upload

---
+++ Cadena de caracteres 1/src/Cadenadecaracteres1/Main.java
@@ -0,0 +1,19 @@
+package Cadenadecaracteres1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		var sc=new Scanner(System.in);
+		
+		System.out.println("Ingrese nombre: ");
+		var nombre = sc.nextLine();
+		
+		System.out.println("Tu nombre es " + nombre);
+		
+		sc.close();
+	}
+
+}
